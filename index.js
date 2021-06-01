cf9fb3d926e2d74ed301c5fe9577687a71939945
Mike-Monta <Grund1sc@gmail.com>
2021-06-01 22:00:07 +0200

    Se cambia al uso de clicks

--- index.js
+++ index.js
@@ -13,61 +13,85 @@ function computerPlay(){
 }
  // Play a single round with User seleccion and computer gameplay
 
- function playRound (playerSelection, computerSelection){
-    //let winner = "";
+ function playRound (playerSelection){
+    winner = "";
+    let computerSelection= computerPlay();
     
-    playerSelection = window.prompt("Choose one: rock, scissors, or paper");
-    playerSelect = playerSelection.toLowerCase();
-    console.log("User: " + playerSelect + " -  Computer: "+ computerSelection)
-    console.log(playerSelect);
-    if  (playerSelection === "rock"){
-        computerSelection === "rock" ? winner = "Tie"
-        : computerSelection === "scissors"? winner= "User"
-        : winner = "Computer";
-        
-        
+    console.log("User: " + playerSelection + " -  Computer: "+ computerSelection)
+    
+    if (playerSelection === computerSelection){
+        ties += 1;
+        document.getElementById("ties").innerHTML= ties;
+    }else if  (playerSelection === "rock"){
+          if (computerSelection === "scissors"){
+            userScore += 1;
+            document.getElementById("userResult").innerHTML= userScore;
+          }else{
+            computerScore += 1;
+            document.getElementById("computerResult").innerHTML= computerScore;
+          }
     }else if (playerSelection === "paper"){
-        computerSelection === "rock" ? winner = "User"
-        : computerSelection === "scissors"? winner= "Computer"
-        : winner = "Tie";
-        
-       
+        if (computerSelection === "rock"){
+            userScore += 1;
+            document.getElementById("userResult").innerHTML= userScore;
+          }else{
+            computerScore += 1;
+            document.getElementById("computerResult").innerHTML= computerScore;
+          }
     } else {
-        computerSelection === "paper" ? winner = "User"
-        : computerSelection === "scissors"? winner= "Tie"
-        : winner = "Computer";
-        
-        
-        
-    }    
-        
+        if (computerSelection === "paper"){
+            userScore += 1;
+            document.getElementById("userResult").innerHTML= userScore;    
+        }else {
+            computerScore += 1;
+            document.getElementById("computerResult").innerHTML= computerScore;
+        }
+    } 
+     
     //console.log(winner);
-    return winner;
-
+    //return winner;
  }
+ 
  // Function  that plays n rounds of playRound()
  function game(n){
-   let userScore= 0;
-   let computerScore = 0;
-   let ties = 0;
+    userScore = 0;
+    computerScore = 0;
+    ties = 0;
+    document.getElementById("userResult").innerHTML= 0;
+    document.getElementById("computerResult").innerHTML= 0;
+    document.getElementById("ties").innerHTML= 0;
    let finalWinner ="";
-    for (i=1; i<n+1; i++){
-        const computerSelection = computerPlay();
-        playRound(playerSelection,computerSelection);
-        winner === "User"? userScore +=1
-        : winner ==="Computer" ? computerScore +=1
-        : ties +=1;
+   while(userScore <= 5 || computerScore <=5 || ties <=5){
+        if (userScore = 5){
+            alert("YOU WIN");
+        }else if (computerScore =5){
+            alert("COMPUTER WINS");
+        }else if( ties = 5){
+            alert("IT'S a TIE");
+        }
+    }
+        
         console.log(playerSelection);
         console.log("Round "+ i + "- User: " + playerSelect + "  - Computer: "+ computerSelection + "   winner: " +winner); 
         console.log( "User: "+ userScore + "   Computer: "+ computerScore+ "   Ties: "+ ties);
     }
 
 
+
+
+function getWinner(){
+    console.log(winner);
 }
+let userScore= 0;
+let computerScore = 0;
+let ties = 0;
  let winner ="";
  const nveces= 5;
- const playerSelection = "";
+// const playerSelection = "";
  let playerSelect ="";
+ const userWinOne = document.getElementById("userResult");
+ const computerWinOne = document.getElementById("computerResult");
+
  //window.prompt("Choose one: rock, scissors, or paper");
- const computerSelection = computerPlay();
+ //const computerSelection = computerPlay();
  //console.log(playRound(playerSelection, computerSelection));
